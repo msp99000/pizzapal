5ea84ee9f9bdd6923aa17208495d98efda7d943b
msp99000 <msp99000@gmail.com>
2024-09-01 17:48:13 +0530

    chore (build): home screen widget definition

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PizzaPal extends StatelessWidget {
+  const PizzaPal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        title: "PizzaPal", home: Center(child: Text("Hunter")));
+  }
+}
