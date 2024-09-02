4039465f57acbc37180902fa139aaddabb160aef
msp99000 <msp99000@gmail.com>
2024-09-03 00:01:58 +0530

    feat (style): modifies SplashScreenController

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pizzapal/app/welcome_svg.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:re_svg/re_svg.dart';
 
 class PizzaPal extends StatelessWidget {
   const PizzaPal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: "PizzaPal", home: Center(child: Text("Hunter")));
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+        title: "PizzaPal",
+        home: Scaffold(
+            body: Center(
+                child: SvgPicture.asset(
+          "assets/images/welcome1.svg",
+        ))));
   }
 }
